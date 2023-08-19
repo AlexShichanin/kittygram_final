221b8001179d90832ef4ec8c6f29bd5b51b5d28d
Алексанр Шичанин <9shichanin@gmail.com>
2023-08-19 20:28:40 +0300

    fix settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*', 'kittyprodd.ddns.net',]
+ALLOWED_HOSTS = ['http://158.160.3.3:9000', '127.0.0.1', 'localhost', 'kittyprodd.ddns.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
